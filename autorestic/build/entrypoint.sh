34acbace0d4c2473bf77ce9957a05c3408a3cdcc
Tyler Moore <TylerRMoore@gmail.com>
2022-09-23 03:04:56 -0700

    Initial commit

---
+++ autorestic/build/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo
+echo "Initializing path"
+PATH="/usr/local/bin:/usr/bin:/bin"
+
+echo
+echo "Substituting environment variables into config file"
+envsubst < /autorestic_preconfig.yml > /autorestic.yml
+
+echo
+echo "Autorestic check"
+autorestic check -c ./autorestic.yml
+
+echo
+echo "Running backup on startup"
+autorestic backup -c ./autorestic.yml
+
+echo
+echo "Entering infinite loop"
+while true
+do
+    echo "Running cron."
+    $CRON_LOOP_START_HOOK
+    autorestic -c ./autorestic.yml --ci cron > /tmp/autorestic.log 2>&1
+    $CRON_LOOP_FINISH_HOOK
+    echo "Cron finished. Log file:"
+    echo $(cat /tmp/autorestic.log)
+    rm /tmp/autorestic.log
+    echo "Sleeping."
+    sleep 15m
+done
