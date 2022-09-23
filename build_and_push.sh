dedfd145ede5d877a066d49d3ba14db1aa8bbbe8
Tyler Moore <TylerRMoore@gmail.com>
2022-09-23 03:37:38 -0700

    Build and push script

---
+++ build_and_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+REGISTRY=docker.io
+IMAGE_NAME=autorestic
+UNAME=overtone1000
+
+TAG=$REGISTRY/$UNAME/$IMAGE_NAME
+docker login
+docker build -t $IMAGE_NAME ./build
+docker image tag $IMAGE_NAME $TAG
+docker push $TAG
